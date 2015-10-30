d5f881bd2410a14b0997381d505b14919f204059
g-un-- <office.gino@gmail.com>
2015-10-30 13:07:42 +0200

    Demo of finite state machine build with a recursive delegate and F# agents for thread safety.

---
+++ FiniteStateMachineDemo/CodeLock.cs
@@ -0,0 +1,59 @@
+﻿using ActionAgent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateMachineDemo
+{
+    public delegate Handle<T> Handle<T>(string message);
+
+    public class CodeLock
+    {
+        private Handle<string> state;
+        private Agent<string> agent;
+
+        public CodeLock()
+        {
+            state = Locked();
+            agent = new Agent<string>(message =>
+            {
+                state = state(message);
+            });
+        }
+
+        public void Post(string message)
+        {
+            agent.Post(message);
+        }
+
+        Handle<string> Unlocked()
+        {
+            return new Handle<string>(message =>
+            {
+                if (message == "lock")
+                    return Locked();
+                else
+                {
+                    Console.WriteLine(message);
+                    return Unlocked();
+                }
+            });
+        }
+
+        Handle<string> Locked()
+        {
+            return new Handle<string>(message =>
+            {
+                if (message == "key")
+                    return Unlocked();
+                else
+                {
+                    Console.WriteLine("Nice try!");
+                    return Locked();
+                }
+            });
+        }
+    }
+}
---
+++ FiniteStateMachineDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateMachineDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeLock = new CodeLock();
+            codeLock.Post("test");
+            codeLock.Post("key");
+            codeLock.Post("hello world!");
+            codeLock.Post("lock");
+            codeLock.Post("test");
+            Console.ReadLine();
+        }
+    }
+}
